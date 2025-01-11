84f4fa217525bfc2d093b1b8d78d775ce805dc25
GiuliL <giulilaurella@gmail.com>
2025-01-11 20:36:21 -0300

    Actualizar App.jsx

--- App.jsx
+++ App.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import ItemListContainer from './components/ItemListContainer';
+import ItemDetailContainer from './components/ItemDetailContainer';
 
 function App() {
     return (
-        <div>
+        <Router>
             <NavBar />
-            <ItemListContainer greeting="Bienvenido a la tienda" />
-        </div>
+            <Routes>
+                <Route path="/" element={<ItemListContainer greeting="Bienvenido a la tienda" />} />
+                <Route path="/category/:categoryId" element={<ItemListContainer />} />
+                <Route path="/item/:itemId" element={<ItemDetailContainer />} />
+            </Routes>
+        </Router>
     );
 }
 
 export default App;
-
